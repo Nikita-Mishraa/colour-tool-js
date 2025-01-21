64a0b83dfdd09d1bba3de426db2e057e70e01a07
= <nikitamishra23062002@gmail.com>
2025-01-21 22:52:12 +0530

    css file

---
+++ index.js
@@ -0,0 +1,20 @@
+const hexInput = document.getElementById('hexInput');
+const inputColour = document.getElementById('inputColour');
+
+hexInput.addEventListener('keyup', () =>{
+    const hex = hexInput.value;
+    if(! isValidHex(hex)) return;
+
+    const strippedHex = hex.replace('#', '');
+
+
+    inputColour.style.background = "#" + strippedHex;
+})
+
+
+const isValidHex = (hex) => {
+    if(!hex) return false;
+    
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}

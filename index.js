e89b062a600d553bc4b834e657a822210034193e
= <nikitamishra23062002@gmail.com>
2025-01-22 01:01:23 +0530

    h to r

--- index.js
+++ index.js
@@ -29,4 +29,19 @@ const convertHexToRGB = (hex) =>{
     + strippedHex[1] + strippedHex[1]
     + strippedHex[2] + strippedHex[2];
   }
+
+  const r  = parseInt(strippedHex.substring(0,2), 16);
+  const g  = parseInt(strippedHex.substring(2,4), 16);
+  const b  = parseInt(strippedHex.substring(4,6), 16);
+  
+  return {r,g,b}
+}
+
+const convertRGBToHex = (r,g,b) => {
+  const firstPair = ("0" + r.toString(16)).slice(-2);
+  const secondPair = ("0" + g.toString(16)).slice(-2);
+  const thirdPair = ("0" + b.toString(16)).slice(-2);
+  
+  const hex = "#" + firstPair + secondPair + thirdPair;
+  return hex;
 }

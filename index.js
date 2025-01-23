11d1084df4c4dba2837af551231a789af964e5c9
= <nikitamishra23062002@gmail.com>
2025-01-24 03:42:21 +0530

    toggle button added

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const sliderText = document.getElementById('sliderText');
 const slider = document.getElementById('slider');
 const alteredColour = document.getElementById('alteredColour');
 const alteredColourText = document.getElementById('alteredColourText');
+const lightenText = document.getElementById('lightenText');
+const darkenText = document.getElementById('darkenText');
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', ()=>{
+  if(toggleBtn.classList.contains('toggled')){
+    toggleBtn.classList.remove('toggled');
+    lightenText.classList.remove('unselected');
+    darkenText.classList.add('unselected');
+  } else{
+    toggleBtn.classList.add('toggled');
+    lightenText.classList.add('unselected');
+    darkenText.classList.remove('unselected');
+  }
+})
+
+
 
 hexInput.addEventListener('keyup', () =>{
     const hex = hexInput.value;
